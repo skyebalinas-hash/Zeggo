e6e0883173aee83468a722de94a5f9d4cc1523d2
skyebalinas-hash <skyebalinas@gmail.com>
2026-07-17 12:33:05 +0800

    data.js

---
+++ data.js
@@ -0,0 +1,99 @@
+const properties=[
+
+{
+
+id:1,
+
+city:"cebu",
+
+title:"Modern Family Home",
+
+price:"₱5,500,000",
+
+bedrooms:3,
+
+bathrooms:2,
+
+garage:2,
+
+status:"For Sale",
+
+type:"House",
+
+image:"https://images.unsplash.com/photo-1568605114967-8130f3a36994?w=900"
+
+},
+
+{
+
+id:2,
+
+city:"manila",
+
+title:"Luxury Condo",
+
+price:"₱8,900,000",
+
+bedrooms:2,
+
+bathrooms:2,
+
+garage:1,
+
+status:"For Sale",
+
+type:"Condo",
+
+image:"https://images.unsplash.com/photo-1600585154526-990dced4db0d?w=900"
+
+},
+
+{
+
+id:3,
+
+city:"davao",
+
+title:"Mountain Villa",
+
+price:"₱7,300,000",
+
+bedrooms:4,
+
+bathrooms:3,
+
+garage:2,
+
+status:"For Sale",
+
+type:"House",
+
+image:"https://images.unsplash.com/photo-1512917774080-9991f1c4c750?w=900"
+
+},
+
+{
+
+id:4,
+
+city:"palawan",
+
+title:"Beachfront Villa",
+
+price:"₱15,000,000",
+
+bedrooms:5,
+
+bathrooms:4,
+
+garage:3,
+
+status:"For Sale",
+
+type:"Villa",
+
+image:"https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?w=900"
+
+}
+
+];

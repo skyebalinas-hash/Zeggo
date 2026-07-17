d08a0ecd8957d784208d7d77b3c36240ae11d879
skyebalinas-hash <skyebalinas@gmail.com>
2026-07-17 20:50:40 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 const properties = [
+
     {
         id: 1,
         title: "Modern Family Home",
@@ -30,5 +31,39 @@ const properties = [
         bathrooms: 1,
         type: "Apartment",
         image: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267"
+    },
+
+    {
+        id: 4,
+        title: "Mountain View Villa",
+        city: "Baguio",
+        price: "₱12,000,000",
+        bedrooms: 5,
+        bathrooms: 4,
+        type: "Villa",
+        image: "https://images.unsplash.com/photo-1600607687939-ce8a6c25118c"
+    },
+
+    {
+        id: 5,
+        title: "Cozy Starter Home",
+        city: "Iloilo",
+        price: "₱2,800,000",
+        bedrooms: 2,
+        bathrooms: 1,
+        type: "House",
+        image: "https://images.unsplash.com/photo-1570129477492-45c003edd2be"
+    },
+
+    {
+        id: 6,
+        title: "Luxury Modern Apartment",
+        city: "Makati",
+        price: "₱6,700,000",
+        bedrooms: 3,
+        bathrooms: 2,
+        type: "Apartment",
+        image: "https://images.unsplash.com/photo-1493809842364-78817add7ffb"
     }
+
 ];
